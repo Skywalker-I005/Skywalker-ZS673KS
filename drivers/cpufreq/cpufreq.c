b028d99b31adc3e886f0352f3d2a67c79b630139
Abandoned Cart <twistedumbrella@gmail.com>
2021-09-27 10:31:40 -0400

    Revert " cpufreq: notify of CPU utilization at max frequency"
    This reverts commit 6954b287cdca38e193c0961963989dc7569aaff3.

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -643,21 +643,6 @@ unsigned int cpufreq_policy_transition_delay_us(struct cpufreq_policy *policy)
 }
 EXPORT_SYMBOL_GPL(cpufreq_policy_transition_delay_us);
 
-/**
- * cpufreq_notify_utilization - notify CPU userspace about CPU utilization change
- *
- * This function is called everytime the CPU load is evaluated by the
- * ondemand governor. It notifies userspace of cpu load changes via sysfs.
- */
-void cpufreq_notify_utilization(struct cpufreq_policy *policy, unsigned int util)
-{
-	if (policy)
-		policy->util = util;
-
-	if (policy->util >= MIN_CPU_UTIL_NOTIFY)
-		sysfs_notify(&policy->kobj, NULL, "cpu_utilization");
-}
-
 /*********************************************************************
  *                          SYSFS INTERFACE                          *
  *********************************************************************/
@@ -766,7 +751,6 @@ show_one(cpuinfo_min_freq, cpuinfo.min_freq);
 show_one(cpuinfo_transition_latency, cpuinfo.transition_latency);
 show_one(scaling_min_freq, min);
 show_one(scaling_max_freq, max);
-show_one(cpu_utilization, util);
 
 unsigned int cpuinfo_max_freq_cached;
 
@@ -1019,7 +1003,6 @@ cpufreq_freq_attr_ro(scaling_cur_freq);
 cpufreq_freq_attr_ro(bios_limit);
 cpufreq_freq_attr_ro(related_cpus);
 cpufreq_freq_attr_ro(affected_cpus);
-cpufreq_freq_attr_ro(cpu_utilization);
 cpufreq_freq_attr_rw(scaling_min_freq);
 cpufreq_freq_attr_rw(scaling_max_freq);
 cpufreq_freq_attr_rw(scaling_governor);
@@ -1032,7 +1015,6 @@ static struct attribute *default_attrs[] = {
 	&scaling_min_freq.attr,
 	&scaling_max_freq.attr,
 	&affected_cpus.attr,
-	&cpu_utilization.attr,
 	&related_cpus.attr,
 	&scaling_governor.attr,
 	&scaling_driver.attr,
